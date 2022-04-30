169fa841656fd5aeb2d89fa727aeff4561cec5b4
Rishika Gupta <73276425+rishika-afk@users.noreply.github.com>
2022-04-30 10:45:11 +0530

    Update pointer-basic.c

--- pointer-basic.c
+++ pointer-basic.c
@@ -1,10 +1,26 @@
+#include<stdio.h>
+
+int main(){
 int a=10;
 int b=10;
 int *p;                                   //Pointer is variable that refers to address of another variable
 p = &a;                                   //Pointing p to address of 'a'
 printf("%d", a);                          //10
-printf("%u %u", &a,p);                    //Address of 'a'
+printf("%p %p", &a,p);                    //Address of 'a'
 printf("%d %d",*&a,*p);                   //10 10
 p=&b;                                     //Pointer p now points to address of 'b'
 *p=20;                                    //value of 'b' changed to 20
 printf("%d %d", a,*p);                    //10 20
+
+
+//Many pointers can refer to the same variable's address location
+int x;
+int *q,*r;
+q = &x;
+r = &x;
+printf("%d %d %d",a *p,*q);    //10 10 10
+*q = 20;                       // Value oprintf("%d %d %d",a *p,*q);    //10 10 10f variable 'x' changes to 20
+printf("%d %d %d",a *p,*q);    //20 20 20
+  
+return 0;
+}
