e5b42e7d3b9d91dfb5fae8c313ef606a3b96fa69
Rishika Gupta <73276425+rishika-afk@users.noreply.github.com>
2022-05-02 09:32:41 +0530

    Update strlen.c

--- strlen.c
+++ strlen.c
@@ -1,5 +1,6 @@
 //logic'function for strlen to find length of string 
+strlen(str1);
 char str1[]="India";
-  int i=0;
-  while(str[i]!='\0')i++;
-  printf(%d",i);
+int i=0;
+while(str[i]!='\0')i++;
+printf(%d",i);
